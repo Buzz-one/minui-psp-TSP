0262fc672cf20762efcd20b7820d50339bc0600a
Buzz-one <chik1122@hotmail.com>
2025-08-25 11:05:34 +0200

    Support dynamic .pak naming for PSP emulator
    Previously, the script hardcoded the package path as:
      $SDCARD_PATH/Emus/$PLATFORM/PSP.pak/PPSSPPSDL
    
    This assumed the .pak file was always named "PSP.pak".
    Now, the path is built dynamically from the current directory
    and .pak name, e.g. $PAK_NAME.pak, allowing users to rename
    the .pak file without breaking the launcher.
    
    This adds flexibility while remaining compatible with the
    default store-provided "PSP.pak".

--- launch.sh
+++ launch.sh
@@ -17,8 +17,9 @@ if uname -m | grep -q '64'; then
     architecture=arm64
 fi
 
-export PAK_DIR="$SDCARD_PATH/Emus/$PLATFORM/PSP.pak"
-export EMU_DIR="$SDCARD_PATH/Emus/$PLATFORM/PSP.pak/PPSSPPSDL"
+# Dinamic .PAK name that allow the user to change the name if required
+export PAK_DIR="$SDCARD_PATH/Emus/$PLATFORM/$PAK_NAME.pak"
+export EMU_DIR="$PAK_DIR/PPSSPPSDL"
 
 export PATH="$EMU_DIR:$PAK_DIR/bin/$architecture:$PAK_DIR/bin/$PLATFORM:$PAK_DIR/bin:$PATH"
 export HOME="$EMU_DIR"
